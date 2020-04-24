1404aaf30af27f638f03ebc3aa11737522ac88cf
bciobanu <11034715+bciobanu@users.noreply.github.com>
2020-04-24 15:28:14 +0300

    Add error banner

--- client/src/Submit.js
+++ client/src/Submit.js
@@ -2,6 +2,9 @@ import React from "react";
 import Editor, {LANGUAGE_LIST} from "./Editor";
 import {Select, MenuItem, Input} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 import "./Submit.css";
@@ -12,8 +15,9 @@ export default class Submit extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {languageId: 0, privacyOptionId: 0, code: ""};
+        this.state = {languageId: 0, privacyOptionId: 0, code: "", errorMessage: null};
         this.handleChange = this.handleChange.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
     componentDidMount() {
@@ -44,6 +48,10 @@ export default class Submit extends React.Component {
         }
     }
 
+    handleClose() {
+        this.setState((previousState) => Object.assign({}, previousState, {errorMessage: null}));
+    }
+
     render() {
         const languageOptions = LANGUAGE_LIST.map((langName, index) => (
             <MenuItem value={index}>{langName}</MenuItem>
@@ -51,37 +59,58 @@ export default class Submit extends React.Component {
         const privacyOptions = Submit.PRIVACY_OPTIONS.map((privacyOption, index) => (
             <MenuItem value={index}>{privacyOption}</MenuItem>
         ));
-        const {languageId, privacyOptionId, code} = this.state;
+        const {languageId, privacyOptionId, code, errorMessage} = this.state;
 
         return (
-            <div className="submit">
-                <Editor
-                    readOnly={false}
-                    code={code}
-                    language={languageId}
-                    onChange={(editor, data, value) => {
-                        if (!localStorage) {
-                            return;
-                        }
-                        localStorage.setItem(Submit.STORAGE_KEY, value);
-                        this.setState((previousState) =>
-                            Object.assign({}, previousState, {code: value})
-                        );
+            <>
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "center",
                     }}
+                    open={errorMessage !== null}
+                    autoHideDuration={6000}
+                    onClose={this.handleClose}
+                    message={errorMessage}
+                    action={
+                        <IconButton
+                            size="small"
+                            aria-label="close"
+                            color="inherit"
+                            onClick={this.handleClose}>
+                            <CloseIcon fontSize="small" />
+                        </IconButton>
+                    }
                 />
-                <div className="options">
-                    <Select name="language" value={languageId} onChange={this.handleChange}>
-                        {languageOptions}
-                    </Select>
-                    <Select name="privacy" value={privacyOptionId} onChange={this.handleChange}>
-                        {privacyOptions}
-                    </Select>
-                    <Input defaultValue="Key" disabled={privacyOptionId === 0} />
-                    <Button className="button" color="default" startIcon={<CloudUploadIcon />}>
-                        Upload
-                    </Button>
+                <div className="submit">
+                    <Editor
+                        readOnly={false}
+                        code={code}
+                        language={languageId}
+                        onChange={(editor, data, value) => {
+                            if (!localStorage) {
+                                return;
+                            }
+                            localStorage.setItem(Submit.STORAGE_KEY, value);
+                            this.setState((previousState) =>
+                                Object.assign({}, previousState, {code: value})
+                            );
+                        }}
+                    />
+                    <div className="options">
+                        <Select name="language" value={languageId} onChange={this.handleChange}>
+                            {languageOptions}
+                        </Select>
+                        <Select name="privacy" value={privacyOptionId} onChange={this.handleChange}>
+                            {privacyOptions}
+                        </Select>
+                        <Input defaultValue="Key" disabled={privacyOptionId === 0} />
+                        <Button className="button" color="default" startIcon={<CloudUploadIcon />}>
+                            Upload
+                        </Button>
+                    </div>
                 </div>
-            </div>
+            </>
         );
     }
 }
